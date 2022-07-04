1086f45c9eb172e9137431df37a6ccb684739dff
Vikrant Bhat <vik.bhat223@gmail.com>
2022-07-04 15:46:17 +0530

    chore: add launch.json for debugging

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 function sayMyName(name: string): void {
   if (name === "Heisenberg") {
-    console.log("You're right 👍");
+    console.log("You're right 👍!!");
   } else {
     console.log("You're wrong 👎");
   }
