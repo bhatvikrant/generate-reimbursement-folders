ab9e22d95b9bc4af33a4ee9ffde07f3d994afae4
Vikrant Bhat <vik.bhat223@gmail.com>
2022-07-05 09:28:12 +0530

    chore: add fs-extra package

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,38 @@
-function sayMyName(name: string): void {
-  if (name === "Heisenberg") {
-    console.log("You're right 👍!!");
-  } else {
-    console.log("You're wrong 👎");
+import fse from "fs-extra";
+import { exec } from "child_process";
+
+const PARENT_FOLDER = "reimbursements";
+
+function generateFolders() {
+  console.log("STARTING");
+  try {
+    if (!fse.existsSync(PARENT_FOLDER)) {
+      fse.mkdirSync(PARENT_FOLDER);
+      fse.mkdirSync(`/${PARENT_FOLDER}/test`);
+
+      exec("cd reimbursements && ls -la", (error, stdout, stderr) => {
+        if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+      });
+    }
+  } catch (err) {
+    console.error(err);
   }
+  // const folderName = "reimbursements";
+  // try {
+  //   if (!fse.existsSync(folderName)) {
+  //     fse.mkdirSync(folderName);
+  //   }
+  // } catch (err) {
+  //   console.error(err);
+  // }
 }
 
-sayMyName("Heisenberg");
+generateFolders();
---
+++ src/static-data.ts
@@ -0,0 +1,35 @@
+export const years = [2022, 2023];
+
+export const months = [
+  "Janurary",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export const subFolders = [
+  {
+    name: "Communication",
+    icon: "./folder-icons/wifi-signal.png",
+  },
+  {
+    name: "Fuel",
+    icon: "./folder-icons/gas-station.png",
+  },
+  {
+    name: "Health & Fitness",
+    icon: "./folder-icons/fitness.png",
+  },
+  {
+    name: "LTA",
+    icon: "./folder-icons/honeymoon.png",
+  },
+];
