a7b55b4a78f791355d65fdae732683925b5aed22
Vikrant Bhat <vik.bhat223@gmail.com>
2022-07-05 18:37:36 +0530

    chore: update main.ts to run the bash script

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,16 @@
 import fse from "fs-extra";
-import { exec } from "child_process";
+import { execSync } from "child_process";
 
-const PARENT_FOLDER = "reimbursements";
+const PARENT_FOLDER = "TOTO";
 
 function generateFolders() {
-  console.log("STARTING");
   try {
     if (!fse.existsSync(PARENT_FOLDER)) {
-      fse.mkdirSync(PARENT_FOLDER);
-      fse.mkdirSync(`/${PARENT_FOLDER}/test`);
-
-      exec("cd reimbursements && ls -la", (error, stdout, stderr) => {
-        if (error) {
-          console.log(`error: ${error.message}`);
-          return;
-        }
-        if (stderr) {
-          console.log(`stderr: ${stderr}`);
-          return;
-        }
-        console.log(`stdout: ${stdout}`);
-      });
+      execSync(`./bash/gen-folders.sh`, { stdio: "inherit" });
     }
   } catch (err) {
     console.error(err);
   }
-  // const folderName = "reimbursements";
-  // try {
-  //   if (!fse.existsSync(folderName)) {
-  //     fse.mkdirSync(folderName);
-  //   }
-  // } catch (err) {
-  //   console.error(err);
-  // }
 }
 
 generateFolders();
